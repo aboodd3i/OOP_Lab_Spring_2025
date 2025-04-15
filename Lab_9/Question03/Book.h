99a0bdf2ba6b753b58dcc4c96058f46a61290bda
Abdullah <abdullahmuhammad0610@gmail.com>
2025-04-15 09:01:51 +0500

    Create Book.h

---
+++ Lab_9/Question03/Book.h
@@ -0,0 +1,16 @@
+#ifndef BOOK_H
+#define BOOK_H
+
+class Book {
+private:
+string title, author, ISBN;
+
+public:
+Book(string title, string author, string ISBN) {}
+
+string getTitle() {}
+string getAuthor() {}
+string getISBN() {}
+};
+
+#endif

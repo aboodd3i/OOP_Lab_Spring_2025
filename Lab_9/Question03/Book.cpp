ce35e218b073c9aaf1c8324d85ddb61f27579d28
Abdullah <abdullahmuhammad0610@gmail.com>
2025-04-18 23:14:01 +0500

    Update Book.cpp

--- Lab_9/Question03/Book.cpp
+++ Lab_9/Question03/Book.cpp
@@ -1,12 +1,63 @@
+#include "BookH.hpp"
 #include "iostream"
-#include "Book.h"
+
 using namespace std;
+Book::Book(){
+  title =" ";
+  author = " ";
+  ISBN = " ";
+}
+Book::Book(string t,string a,string i): title(t), author(a), ISBN(i) {}
+Book::string getTitle(){
+	return title;
+}
+Book::string getAuthor(){
+  	return author;
+}
+Book::string getISBN(){
+  	return ISBN;
+}
+Library::Library(){
+	count = 0;
+}
+Library::void addBook(Book& b1){
+	if(count<20){
+    	books[count] = b1;
+    	count++;
+  	}
+  	else{
+    	cout<<"No space left to add another book!"<<endl;
+  	}
+}
 
-Book::Book(string title, string author, string ISBN) : title(title), author(author), ISBN(ISBN) {}
+Library::void removeBook(Book& b1){
+    int flag=0;
+    for(int i=0;i<count;i++){
+        if(books[count].getISBN() == b1.getISBN()){
+          count--;
+          cout<<"Book removed!"<<endl;
+          flag =1;
+        }  
+    }
+  if(flag == 1){
+    cout<<"Book "<<b1.getTitle()<<" has been removed!"<<endl;
+  }
+  else{
+    cout<<"Book "<<b1.getTitle()<<" was not found!"<<endl;
+  }
+}
 
-cout << "displaying details of the Book: " << endl << "Author: " << Book::getAuthor() << endl << "Book Name: " << Book::getTitle() << endl << "ISBN: " << Book::getISBN() << endl;
-
-string getTitle() { return title; }
-string getAuthor() { return author; }
-string getISBN() { return ISBN; }
-
+Library::void searchBook(Book &b1){
+	int flag=0;
+    for(int i=0;i<count;i++){
+        if(books[count].getISBN() == b1.getISBN()){
+          flag =1;
+        }  
+    }
+    if(flag == 1){
+    	cout<<"Book "<<b1.getTitle()<<" has been found!"<<endl;
+  	}
+  	else{
+    	cout<<"Book "<<b1.getTitle()<<" was not found!"<<endl;
+  	}
+}
